5c3d834b90e9cfebb14c57fadd11874b597cd4d1
marrasluca <lmarras10@gmail.com>
2025-01-24 19:18:12 +0100

    add: function js

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const btn = document.getElementById('bottone')
 
 btn.addEventListener('click', function() {
-    const nome = getElementById('nome').value
-    const eta = getElementById( 'fascia.eta').value
-    const km = getElementById('km').value
+    const nome = document.getElementById( 'nome' ).value
+    const eta = document.getElementById( 'fascia-eta').value
+    const km = document.getElementById( 'km' ).value
 
     console.log (nome, eta, km)
 
-    constBigliettoFinale = document.getElementById('biglietto')
+    const bigliettoFinale = document.getElementById('biglietto')
 
     bigliettoFinale.classList.remove('hidden')
     bigliettoFinale.classList.add('show')
+
+    const nomePasseggero = document.getElementById('nome-passeggero')
+    const offertaApplicata = document.getElementById('offerta-applicata')
+
+    nomePasseggero.innerHTML = nome
+
+    if( eta == "maggiorenne" ){
+        offertaApplicata.innerHTML = "Biglietto Standard"
+
+    }else if ( eta == "minorenne"){
+        offertaApplicata.innerHTML = "Biglietto Ridotto"
+        
+    }else{
+        offertaApplicata.innerHTML = "Biglietto over 65"
+    }
 
 })
