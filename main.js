c05e2c4b8631f2a77e263faaada3d1a2e15e087b
marrasluca <lmarras10@gmail.com>
2025-01-25 10:34:22 +0100

    end

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const btn = document.getElementById('bottone')
+const prezzo = 0.21
 
 btn.addEventListener('click', function() {
     const nome = document.getElementById( 'nome' ).value
     const eta = document.getElementById( 'fascia-eta').value
     const km = document.getElementById( 'km' ).value
+    const costo = document.getElementById('costo')
 
     console.log (nome, eta, km)
 
@@ -19,12 +21,22 @@ btn.addEventListener('click', function() {
 
     if( eta == "maggiorenne" ){
         offertaApplicata.innerHTML = "Biglietto Standard"
-
+        costo.innerHTML = prezzo * km
     }else if ( eta == "minorenne"){
         offertaApplicata.innerHTML = "Biglietto Ridotto"
-        
+        costo.innerHTML = (prezzo * km) * 0.80
     }else{
         offertaApplicata.innerHTML = "Biglietto over 65"
+        costo.innerHTML = (prezzo * km) * 0.60
     }
 
+    carrozza.innerHTML = randomNumber (1, 99)
+    codice.innerHTML = randomNumber(1111, 9999)
+
+
 })
+
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * max) + min
+}
